e87a5080cf25ac4a7b07cd93c60436d52afcca31
david1o7 <nolimitblaqs@gmail.com>
2025-06-19 15:52:24 +0100

    Add files via upload

---
+++ constants/MenuImages.js
@@ -0,0 +1,16 @@
+
+import bb1 from "@/assets/images/menu/bb1.jpg"
+import bb2 from "@/assets/images/menu/bb2.jpg"
+import bb3 from "@/assets/images/menu/bb3.jpg"
+import bb4 from "@/assets/images/menu/bb4.jpg"
+import bb5 from "@/assets/images/menu/bb5.jpg"
+import bb6 from "@/assets/images/menu/bb6.jpg"
+
+export default[
+    bb4,
+    bb3,
+    bb2,
+    bb6,
+    bb5,
+    bb1
+]
---
+++ constants/MenuItems.js
@@ -0,0 +1,32 @@
+export const MENU_ITEMS = [
+    {
+        "id" : 1,
+        "title" : "NBA Female Jerseys",
+        "description" : " Authentic , Non-stretchy material.",
+    },
+     {
+        "id" : 2,
+        "title" : "Balls",
+        "description" : " Authentic.",
+    },
+     {
+        "id" : 3,
+        "title" : "Mini hoops",
+        "description" : " Authentic.",
+    },
+     {
+        "id" : 4,
+        "title" : "Bigger hoops",
+        "description" : "Authuentic.",
+    },
+     {
+        "id" : 5,
+        "title" : "Accesories",
+        "description" : "Authentic. ",
+    },
+     {
+        "id" : 6,
+        "title" : "NBA Jerseys",
+        "description" : "Authentic , Non-stretchy material.",
+    },
+]
